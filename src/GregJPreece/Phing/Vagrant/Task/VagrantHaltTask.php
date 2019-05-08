5eb38deb200d46952f0446edfbfc7f0fe3e12f6e
Greg J Preece <greg@preece.ca>
2019-05-07 18:12:25 -0700

    Halt task now accepts target machine ID/name.

--- src/GregJPreece/Phing/Vagrant/Task/VagrantHaltTask.php
+++ src/GregJPreece/Phing/Vagrant/Task/VagrantHaltTask.php
@@ -2,14 +2,19 @@
 
 namespace GregJPreece\Phing\Vagrant\Task;
 
+use GregJPreece\Phing\Vagrant\Traits\AcceptsMachineIdentifier;
+
 /**
  * Wrapper for Vagrant's "halt" command
  * @author Greg J Preece <greg@preece.ca>
  */
 class VagrantHaltTask extends VagrantTask {
 
+    use AcceptsMachineIdentifier;
+    
     public function main() {
-        $this->runCommand('halt');
+        $machine = $this->getMachineIdentifier();
+        $this->runCommand('halt ' . $machine);
     }
     
 }
