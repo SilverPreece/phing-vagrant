40914925b311909cbef23bcf62a8ee9f5d41f7e4
Greg J Preece <greg@preece.ca>
2019-05-07 19:15:02 -0700

    Destroy task now accepts target machine ID/name.

--- src/GregJPreece/Phing/Vagrant/Task/VagrantDestroyTask.php
+++ src/GregJPreece/Phing/Vagrant/Task/VagrantDestroyTask.php
@@ -2,14 +2,19 @@
 
 namespace GregJPreece\Phing\Vagrant\Task;
 
+use GregJPreece\Phing\Vagrant\Traits\AcceptsMachineIdentifier;
+
 /**
  * Wrapper for Vagrant's "destroy" command
  * @author Greg J Preece <greg@preece.ca>
  */
 class VagrantDestroyTask extends VagrantTask {
 
+    use AcceptsMachineIdentifier;
+    
     public function main() {
-        $this->runCommand('destroy --force');
+        $machine = $this->getMachineIdentifier();
+        $this->runCommand('destroy ' . $machine . ' --force');
     }
 
 }
